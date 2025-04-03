939ead3f52f8c6834a4020400b6ab79e8f10ee4e
ttimasdf <opensource@rabit.pw>
2025-04-03 15:34:25 +0800

    optimize warning message

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from binaryninja_mcp.plugin import plugin_init
 import binaryninja as bn
 
+# check if binaryninja plugin system is initialized
 if bn.core_ui_enabled():
     plugin_init()
 else:
     import warnings
-    warnings.warn("BinaryNinja is running in Headless mode or accidentally sourced plugin file")
+    warnings.warn("BinaryNinja Plugin is accidentally loaded outside of BinaryNinja")
