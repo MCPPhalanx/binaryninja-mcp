c13e9038915949c00000705d96e7d7e18d99c9f2
ttimasdf <opensource@rabit.pw>
2025-04-08 06:29:27 +0800

    cleanup test code, use default function scope for fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,19 @@
-import pytest
 import binaryninja as bn
+import pytest
+
 from binaryninja_mcp.utils import disable_binaryninja_user_plugins
 
 disable_binaryninja_user_plugins()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def bv():
 	"""Fixture that loads the BNDB for beleaf.elf binary"""
 	bv = bn.load('tests/binary/beleaf.elf.bndb')
 	yield bv
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def bvs(bv):
 	"""Fixture that loads the BNDB and ELF file for beleaf.elf binary"""
 	bv2 = bn.load('tests/binary/beleaf.elf')
